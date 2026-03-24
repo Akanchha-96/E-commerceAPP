3e8c9b358e685da42dfc2aefbdb010ec78482906
Akanksha Jha <akanchhajha96@gmail.com>
2026-03-24 14:59:49 +0530

    uncommented create_tables function

--- backend/run.py
+++ backend/run.py
@@ -24,5 +24,6 @@ def home():
     return "E-commerce Backend is running"
 
 if __name__ == "__main__":
+    create_tables()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
