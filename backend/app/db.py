39f455f35f0d200269b8eb4851e16ece9fbf7497
Akanksha Jha <akanchhajha96@gmail.com>
2026-03-24 14:20:34 +0530

    modifies db.py,config.py and run.py for MySQL provision on cloud

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,5 +2,4 @@ import mysql.connector
 from app.config import DB_CONFIG
 
 def get_db_connection():
-    db = mysql.connector.connect(**DB_CONFIG)
-    return db
+    return mysql.connector.connect(**DB_CONFIG)
--- backend/run.py
+++ backend/run.py
@@ -19,11 +19,6 @@ app.register_blueprint(cart_bp)
 app.register_blueprint(order_bp)
 app.register_blueprint(user_bp)
 
-create_tables()
-
-
-
-   
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
